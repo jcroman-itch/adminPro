30cc4d899cc506cda0550a32c782f32198721215
jc.flores <jc.flores@fds.com.mx>
2019-02-28 12:57:04 -0700

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
@@ -6,35 +5,24 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 
 
+//Modulos
+import { PageModule } from './pages/pages.module';
+
+
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegistrerComponent } from './login/registrer/registrer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NoPageFoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegistrerComponent   
+    RegistrerComponent,       
   ],
-  imports: [
-    BrowserModule,
-    APP_ROUTES
+  imports: [    
+    APP_ROUTES,
+    PageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,13 @@
 import { RouterModule ,Routes } from "@angular/router";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegistrerComponent } from './login/registrer/registrer.component';
 
 
 const appRoutes: Routes = [
-    { 
-        path: '', 
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'graficas1', component: Graficas1Component },   
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-        ]    
-    },
     { path: 'login', component: LoginComponent },
     { path: 'registrer', component: RegistrerComponent },    
-    { path: '**', component: NoPageFoundComponent }
+    { path: '**', component: NoPageFoundComponent }    
 ];
 
 export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash:true } );
